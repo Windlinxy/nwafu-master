83ad0155be7ba15c2f34dab29bc0bb4049e86e36
windlinxy <windlinxy@163.com>
2023-01-28 15:53:02 +0800

    跨域解决、随机问题

---
+++ src/main/java/pers/nwafumaster/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package pers.nwafumaster.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Windlinxy
+ * @description
+ * @date 2023-01-28 12:41
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //放行哪些原始域
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
+
