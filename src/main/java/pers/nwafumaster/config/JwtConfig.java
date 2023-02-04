b0f97ac12f805cdfcde5234b1a8157da0fe7d506
windlinxy <windlinxy@163.com>
2023-02-04 22:37:45 +0800

    优化import

--- src/main/java/pers/nwafumaster/config/JwtConfig.java
+++ src/main/java/pers/nwafumaster/config/JwtConfig.java
@@ -3,11 +3,9 @@ package pers.nwafumaster.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.*;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import pers.nwafumaster.beans.User;
 
--- src/main/java/pers/nwafumaster/interceptor/AuthInterceptor.java
+++ src/main/java/pers/nwafumaster/interceptor/AuthInterceptor.java
@@ -1,7 +1,6 @@
 package pers.nwafumaster.interceptor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
